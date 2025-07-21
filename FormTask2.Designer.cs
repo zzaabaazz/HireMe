dd63762bfb9230fcb1eeb22f0db7047e5b8fe544
Vladimir Belyakov <zzaabaazz@gmail.com>
2025-07-21 19:03:51 +0300

    feat: Выполнено Задание 2
    В `FormTask2.cs` был реализован класс `Server` с методами `GetCount()` и `AddToCount(int value)`, а также UI для демонстрации

--- FormTask2.Designer.cs
+++ FormTask2.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(83, 128);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(172, 88);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(83, 49);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(172, 23);
+            this.buttonRead.TabIndex = 1;
+            this.buttonRead.Text = "buttonRead";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Location = new System.Drawing.Point(83, 78);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(172, 23);
+            this.buttonWrite.TabIndex = 2;
+            this.buttonWrite.Text = "buttonWrite";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // FormTask2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(337, 285);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.listBox1);
+            this.Name = "FormTask2";
             this.Text = "FormTask2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- FormTask2.cs
+++ FormTask2.cs
@@ -1,20 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace HireMe
 {
     public partial class FormTask2 : Form
     {
+        public static class Server
+        {
+            private static int count = 0;
+            public static readonly ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+
+            public static int GetCount()
+            {
+                rwLock.EnterReadLock();
+                try
+                {
+                    return count;
+                }
+                finally
+                {
+                    rwLock.ExitReadLock();
+                }
+            }
+
+            public static void AddToCount(int value)
+            {
+                rwLock.EnterWriteLock();
+                try
+                {
+                    count += value;
+                }
+                finally
+                {
+                    rwLock.ExitWriteLock();
+                }
+            }
+        }
+
         public FormTask2()
         {
             InitializeComponent();
+            buttonRead.Text = "GetCount";
+            buttonWrite.Text = "AddToCount";
+        }
+
+        private async void buttonRead_Click(object sender, EventArgs e)
+        {
+            // Выполняем чтение в фоновом потоке, чтобы не блокировать UI
+            int currentCount = await Task.Run(() => Server.GetCount());
+
+            // Обновляем UI в основном потоке
+            listBox1.Items.Insert(0, $"Read: {currentCount}");
+        }
+
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int value))
+            {
+                // Выполняем запись в фоновом потоке
+                Task.Run(() =>
+                {
+                    Server.AddToCount(value);
+                    // Обновляем UI в основном потоке
+                    this.Invoke(new Action(() =>
+                    {
+                        listBox1.Items.Insert(0, $"Write: {value}");
+                    }));
+                });
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное число.",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
